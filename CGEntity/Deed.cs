fcf44922b557c2fbe95703e893473d4475ebd2da
LucianuSebi <127097933+LucianuSebi@users.noreply.github.com>
2025-04-30 20:11:48 +0300

    added deed class

---
+++ CGEntity/Deed.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CGEntity
+{
+    internal class Deed
+    {
+
+        public int DeedId = 0;
+        public string DeedNumber = "";
+        public string DeedDate = "";
+        public string DeedType = "";
+        public string Authority = "";
+        public string ValueAmount = "";
+        public string ValueCurrency = "";
+        public string Notes = "";
+        private static int counter = 0;
+
+
+        public Deed()
+        {
+            DeedId = ++counter;
+        }
+
+    }
+}
